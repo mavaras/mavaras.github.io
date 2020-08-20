c2a8585c3d6141ee86857c3010d5eabed5be6f3a
Mario Varas <elasgard@hotmail.es>
2020-08-19 21:13:21 -0500

    nu

--- stuff/filters_docs/gencss.sh
+++ stuff/filters_docs/gencss.sh
@@ -1,4 +1,4 @@
-sed -i'.backup' -e "s/_static/\/https:\/\/mavaras.github.io\/stuff\/filters_docs\/static\//g" *.html
+sed -i'.backup' -e "s/_static/\/https:\/\/mavaras.github.io\/stuff\/filters_docs\/static/g" *.html
 sed -i'.backup' -e "s/\/https/https/g" *.html
-sed -i'.backup' -e "s/static\//static/g" *.html
+#sed -i'.backup' -e "s/static\//static/g" *.html
 sed -i'.backup' -e "s/_images/https:\/\/mavaras.github.io\/stuff\/filters_docs\/images/g" *.html
